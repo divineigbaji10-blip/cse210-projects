0cdb40b4b0e11a86673e1d823195d429f46d3b49
divineigbaji10-blip <divineigbaji10@gmail.com>
2026-09-08 15:22:11 +0100

    C# classes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2021;
+        job1._endYear = 2026;
+
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Web Developer";
+        job2._startYear = 2017;
+        job2._endYear = 2024;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
